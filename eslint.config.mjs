24d6c2ffcb983e34c096eddec3034b956f48b43f
sanjauBeni <sanjaysewa999@gmail.com>
2025-08-01 22:22:55 +0530

    feat: Eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// eslint.config.mjs
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    files: ['**/*.ts'],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked
+    ],
+    rules: {
+      'no-console': 'error',
+      'quotes': ['error', 'single'],
+    },  
+});
