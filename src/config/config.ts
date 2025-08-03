3d72176061e7fbc84ae41393243bb30c39566c30
sanjauBeni <sanjaysewa999@gmail.com>
2025-08-03 13:41:21 +0530

    feat: Dotenv-flow setup

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+import DotenvFlow from 'dotenv-flow';
+
+
+DotenvFlow.config();
+
+
+export default {
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    DATABASE_URL: process.env.DATABASE_URL
+}
